e20ce4acfad58d42284beb8bfdd1b6ac9904d4b8
Kshitij Sekhar Dutta <kshitijdutta02@gmail.com>
2025-09-16 17:19:56 +0530

    change 2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const repoName = 'AIAssessmentTool';
 const nextConfig = {
   output: 'export',
+  assetPrefix: `/${repoName}/`,
+  basePath: `/${repoName}`,
 };
 
 module.exports = nextConfig;
